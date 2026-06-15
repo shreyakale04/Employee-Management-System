1427083153fc89671529c08bb247c3ab230515d9
shreyakale08 <kaleshreya347@gmail.com>
2026-06-15 21:10:57 +0530

    connection between Java and MySQL

---
+++ src/util/DBConnection.java
@@ -0,0 +1,32 @@
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+	 private static final String URL = "jdbc:mysql://localhost:3306/employee_management";
+	 private static final String USER = "root";
+	 private static final String PASSWORD = "root";
+	 
+	 public static Connection getConnection(){
+		 
+		 Connection con = null;
+		 
+		 try{
+			 
+			 Class.forName("com.mysql.jdbc.Driver");
+			 
+			 con = DriverManager.getConnection(URL, USER, PASSWORD);
+			 
+			 System.out.println("Database Connected");
+			 
+		 }
+		 catch(Exception e){
+			 
+			 e.printStackTrace();
+		 }
+		 return con;
+	 }
+	
+	
+}
